80b0a0fc0af4da2dce0a57caae6d66ee342ab3d9
B-PROG-droid <ST10470697@RCCONNECT.EDU.ZA>
2026-04-13 19:58:32 +0200

    Add files via upload
    Audio Player file includes the code used to help with the sound of the cyber bot app

---
+++ AudioPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+public class AudioPlayer  
+{
+    private string audioPath;
+
+    public AudioPlayer(string audioPath)
+    {
+        this.audioPath = audioPath;
+    }
+
+    public static void PlayGreeting()
+    {
+        try
+        {
+            string audioPath = "C:\\Users\\Student\\source\\repos\\Part 1 POE\\Part 1 POE\\voice greeter.wav";
+
+            if (File.Exists(audioPath))
+            {
+                var player = new AudioPlayer(audioPath);
+                player.PlaySync();
+            }
+            else
+            {
+                Console.WriteLine("[Voice greeting file not found]");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Could not play audio: {ex.Message}]");
+        }
+    }
+
+    public void PlaySync()
+    {
+        string escapedPath = audioPath.Replace("'", "''");
+
+        var process = new Process();
+        process.StartInfo.FileName = "powershell";
+        process.StartInfo.Arguments = $"-c \"(New-Object Media.SoundPlayer '{escapedPath}').PlaySync()\"";
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.UseShellExecute = false;
+        process.Start();
+        process.WaitForExit();
+    }
+}

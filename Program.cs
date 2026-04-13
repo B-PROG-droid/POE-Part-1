86ca338f0199053f6b7923bfdc2a17129765a4dc
B-PROG-droid <ST10470697@RCCONNECT.EDU.ZA>
2026-04-13 20:01:05 +0200

    Add files via upload
    This is the main program of the code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console setup
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Title = "CyberBot SA - Cybersecurity Awareness Assistant";
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+
+        // Show logo and welcome banner
+        DisplayHelper.ShowLogo();
+
+        // Play voice greeting
+        AudioPlayer.PlayGreeting();
+
+        // Launch chatbot
+        Chatbot bot = new Chatbot();
+        bot.Start();
+    }
+}
